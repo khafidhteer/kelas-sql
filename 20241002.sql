9aa6c283e569865e4ebba0ff063c48d2bf553051
Khafidh Tri Ramdhani <48427041+khafidhteer@users.noreply.github.com>
2024-10-02 12:51:11 +0800

    Update 20241002.sql

--- 20241002.sql
+++ 20241002.sql
@@ -85,3 +85,8 @@ SELECT
 ROUND(SQRT(POWER((MAX(LAT_N) - MIN(LAT_N)),2) +
 POWER((MAX(LONG_W) - MIN(LONG_W)),2)), 4) AS e_dis
 FROM STATION;
+
+-- Population Density Difference
+SELECT 
+MAX(POPULATION) - MIN(POPULATION) AS population_difference
+FROM CITY;
